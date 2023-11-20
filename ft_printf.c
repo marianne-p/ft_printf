d2aec1b7a28c6a8d14449dae6fb0ae5bd87a2b91
Mariana Pihur <110491959+marianne-p@users.noreply.github.com>
2023-11-20 13:14:44 +0000

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -9,10 +9,27 @@ int  ft_printf(const char *str, ...)
   while (str[i])
   {
     if (str[i++] == '%')
-      ft_checker(str[i], ap, i);
+      ft_checker(str[i], ap, &i);
     else
       ft_putchar(str[i]);
     i++;
   }
   va_end(ap);
 }
+
+void  ft_checker(char c, va_list ap, unsigned int *i)
+{
+    if (c == 'c')
+      ft_putchar(va_arg(ap, char), i);
+    else if (c == 's')
+      ft_putstr(va_arg(ap, char *), i);
+    else if (c == 'd')
+      ft_putnbr(va_arg(ap, int), i);
+    else if (c == 'u')
+      ft_putnbr(va_arg(ap, unsigned int), i);
+    //else if (c == 'i')
+    else if (c == 'x')
+      ft_to_hex(va_arg(ap, //??), i);
+    else if (c == 'X')
+      ft_to_hex_up(va_arg(ap, /*hex*/);
+    
