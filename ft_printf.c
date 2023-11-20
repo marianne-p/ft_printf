34db2d885ed9ff8f73cfa53c1c554f204d086e52
Mariana Pihur <110491959+marianne-p@users.noreply.github.com>
2023-11-20 13:24:16 +0000

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -3,13 +3,14 @@ int  ft_printf(const char *str, ...)
   va_list  ap;
   char  *dest;
   unsigned int  i;
+  unsigned int  ret;
 
   va_start(ap, str);
   va_copy(dest, src);
   while (str[i])
   {
     if (str[i++] == '%')
-      ft_checker(str[i], ap, &i);
+      ft_checker(str[i], ap, &i, &ret);
     else
       ft_putchar(str[i]);
     i++;
@@ -17,20 +18,21 @@ int  ft_printf(const char *str, ...)
   va_end(ap);
 }
 
-void  ft_checker(char c, va_list ap, unsigned int *i)
+void  ft_checker(char c, va_list ap, unsigned int *i, unsigned int *ret)
 {
     if (c == 'c')
       ft_putchar(va_arg(ap, char), i);
     else if (c == 's')
-      ft_putstr(va_arg(ap, char *), i);
+      ft_putstr(va_arg(ap, char *), i, ret);
     else if (c == 'd')
-      ft_putnbr(va_arg(ap, int), i);
+      ft_putnbr(va_arg(ap, int), i, ret);
     else if (c == 'u')
-      ft_putnbr(va_arg(ap, unsigned int), i);
+      ft_putnbr(va_arg(ap, unsigned int), i, ret);
     //else if (c == 'i')
+    //else if (c == 'p')
     else if (c == 'x')
-      ft_to_hex(va_arg(ap, long int), i);
+      ft_to_hex(va_arg(ap, long int), i, ret);
     else if (c == 'X')
-      ft_to_hex_up(va_arg(ap, long int), i);
+      ft_to_hex_up(va_arg(ap, long int), i, ret);
 }
     
